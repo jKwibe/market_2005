08f4f729e19973964bb32cf33351ace54f65b1af
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-10 11:17:19 -0600

    finish iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -18,4 +18,45 @@ class Market
   def vendors_that_sell(item)
     @vendors.select {|vendor| vendor.inventory.include?(item)}
   end
+
+  def total_inventory
+
+    all_items = @vendors.flat_map do |ven|
+      ven.inventory.keys
+    end.uniq
+hash ={}
+    all_items.each do |it|
+      @vendors.each do |vendor|
+        s =  @vendors.select{|vendor| vendor.inventory.include?(it)}.map{|ve| ve.inventory}
+        total = 0
+        s.each do  |f|
+          f.each do |k, v|
+            total += v if k == it
+          end
+        end
+        hash[it] = {
+          quantity: total,
+          vendors: @vendors.select{|vendor| vendor.inventory.include?(it)}
+        }
+      end
+    end
+ hash
+  end
+
+  def overstocked_items
+  item = []
+    total_inventory.each do |k, v|
+      item << k if v[:vendors].size > 1 && v[:quantity] > 50
+    end
+    item
+    
+  end
+
+  def sorted_item_list
+    all_items = @vendors.flat_map do |ven|
+      ven.inventory.keys
+    end.uniq
+
+    all_items.map{|it| it.name}.sort
+  end
 end
