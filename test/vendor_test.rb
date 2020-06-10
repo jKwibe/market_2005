451e8f921654a0905e83ce39bf0f2fe59a693043
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-10 09:59:36 -0600

    iteration 2 complete

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,41 +4,59 @@ require './lib/vendor'
 
 class ItemTest < Minitest::Test
   def setup
-    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
-    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
   end
 
   def test_it_exist
-    assert_instance_of Vendor, @vendor
+    assert_instance_of Vendor, @vendor1
   end
 
   def test_it_has_attributes
-    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal "Rocky Mountain Fresh", @vendor1.name
   end
 
   def test_it_has_an_inventory
     expected = {}
-    assert_equal expected, @vendor.inventory
+    assert_equal expected, @vendor1.inventory
   end
 
   def test_it_can_check_stock
-    assert_equal 0, @vendor.check_stock(@item1)
+    assert_equal 0, @vendor1.check_stock(@item1)
   end
 
   def test_it_can_stock
-    @vendor.stock(@item1, 30)
-    assert_equal 30, @vendor.check_stock(@item1)
-    @vendor.stock(@item1, 25)
-    assert_equal 55, @vendor.check_stock(@item1)
-    @vendor.stock(@item2, 12)
-    assert_equal 12, @vendor.check_stock(@item2)
+    @vendor1.stock(@item1, 30)
+    assert_equal 30, @vendor1.check_stock(@item1)
+    @vendor1.stock(@item1, 25)
+    assert_equal 55, @vendor1.check_stock(@item1)
+    @vendor1.stock(@item2, 12)
+    assert_equal 12, @vendor1.check_stock(@item2)
 
     expected = {
       @item1 => 55,
       @item2 => 12
     }
 
-    assert_equal expected, @vendor.inventory
+    assert_equal expected, @vendor1.inventory
+  end
+
+  def test_has_potential_revenue
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    assert_equal 29.75, @vendor1.potential_revenue
+    assert_equal 345.00, @vendor2.potential_revenue
+    assert_equal 48.75, @vendor3.potential_revenue
   end
 end
